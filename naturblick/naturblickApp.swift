3abac002683e46c7aba9a4d1b7c73fa4d5c38e46
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2023-03-21 11:24:52 +0100

    Initial commit of X Code project

---
+++ naturblick/naturblickApp.swift
@@ -0,0 +1,16 @@
+//
+// Copyright © 2023 Museum für Naturkunde Berlin.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct naturblickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
