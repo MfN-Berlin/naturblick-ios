1063d1f0384053fa8a73c0036eb25c5a40c479dc
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2023-03-29 17:00:55 +0200

    Break our selection toggle to its own function

--- naturblick/views/CharactersView.swift
+++ naturblick/views/CharactersView.swift
@@ -9,26 +9,31 @@ struct CharactersView: View {
     let group: Group
     @StateObject private var charactersViewModel = CharactersViewModel()
     @State private var selected: Set<Int64> = []
+
+    private func toggleSelection(character: Character, values: [CharacterValue], id: Int64) {
+        var updated = selected
+
+        if(character.single) {
+            for value in values.filter({$0.id != id}) {
+                updated.remove(value.id)
+            }
+        }
+
+        if updated.contains(id) {
+            updated.remove(id)
+        } else {
+            updated.insert(id)
+        }
+
+        selected = updated
+    }
+
     var body: some View {
         ScrollView {
             VStack {
                 ForEach(charactersViewModel.characters, id: \.0.id) { character, values in
                     CharacterView(character: character, values: values, selected: $selected) { id in
-
-                        var updated = selected
-
-                        if(character.single) {
-                            for value in values.filter({$0.id != id}) {
-                                updated.remove(value.id)
-                            }
-                        }
-
-                        if updated.contains(id) {
-                            updated.remove(id)
-                        } else {
-                            updated.insert(id)
-                        }
-                        selected = updated
+                        toggleSelection(character: character, values: values, id: id)
                     }
                     if character.id != charactersViewModel.characters.last?.0.id {
                         Divider()
