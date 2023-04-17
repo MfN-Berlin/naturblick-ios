c224f6924c6e761a5d78dfb68eecbc6615c1a51b
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2023-04-17 12:08:01 +0200

    add color test view

--- naturblick/utils/Color.swift
+++ naturblick/utils/Color.swift
@@ -27,5 +27,45 @@ extension Color {
     static let error = Color("error")
 
     static let hrColor = Color("hr")
-
+    
+    
+    static let onFeatureButtonPrimary = Color("On_Feature.Button_Primary")
+    static let onFeatureButtonSecondary = Color("On_Feature.Button_Secondary")
+    static let onFeatureDisabled = Color("On_Feature.Disabled")
+    static let onFeatureHighEmphasis = Color("On_Feature.High_Emphasis")
+    static let onFeatureLowEmphasis = Color("On_Feature.Low_Emphasis")
+    static let onFeatureMediumEmphasis = Color("On_Feature.Medium_Emphasis")
+    static let onFeatureSignalHigh = Color("On_Feature.Signal_High")
+    static let onFeatureSignalLow = Color("On_Feature.Signal_Low")
+    static let onFeaturetag = Color("On_Feature.tag")
+    
+    static let onPrimaryButtonPrimary = Color("On_Primary.Button_Primary")
+    static let onPrimaryButtonSecondary = Color("On_Primary.Button_Secondary")
+    static let onPrimaryDisabled = Color("On_Primary.Disabled")
+    static let onPrimaryHighEmphasis = Color("On_Primary.High_Emphasis")
+    static let onPrimaryLowEmphasis = Color("On_Primary.Low_Emphasis")
+    static let onPrimaryMediumEmphasis = Color("On_Primary.Medium_Emphasis")
+    static let onPrimaryMinimumEmphasis = Color("On_Primary.Minimum_Emphasis")
+    static let onPrimarySignalHigh = Color("On_Primary.Signal_High")
+    static let onPrimarySignalLow = Color("On_Primary.Signal_Low")
+    static let onPrimarytag = Color("On_Primary.tag")
+    
+    static let onSecondaryButtonPrimary = Color("On_Secondary.Button_Primary")
+    static let onSecondaryButtonSecondary = Color("On_Secondary.Button_Secondary")
+    static let onSecondaryDisabled = Color("On_Secondary.Disabled")
+    static let onSecondaryHighEmphasis = Color("On_Secondary.High_Emphasis")
+    static let onSecondaryLowEmphasis = Color("On_Secondary.Low_Emphasis")
+    static let onSecondaryMediumEmphasis = Color("On_Secondary.Medium_Emphasis")
+    static let onSecondaryMinimumEmphasis = Color("On_Secondary.Minimum_Emphasis")
+    static let onSecondarySignalHigh = Color("On_Secondary.Signal_High")
+    static let onSecondarySignalLow = Color("On_Secondary.Signal_Low")
+    static let onSecondarySignalMedium = Color("On_Secondary.Signal_Medium")
+    static let onSecondarytag = Color("On_Secondary.tag")
+    static let onSecondarywarning = Color("On_Secondary.warning")
+    
+    static let primaryColor = Color("Primary")
+    static let secondaryColor = Color("Secondary")
+    static let tertiaryColor = Color("tertiary")
+    static let backdropColor = Color("backdrop")
+    static let featureColor = Color("Feature")
 }
