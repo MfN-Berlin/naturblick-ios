ea1a42d2518ad72a455996d3563c132cbda60e7c
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2023-06-02 15:46:51 +0200

    task once

--- naturblick/views/ResultView.swift
+++ naturblick/views/ResultView.swift
@@ -6,6 +6,57 @@
 import SwiftUI
 import SQLite
 
+
+class ResultViewModel : ObservableObject {
+    
+    @Published private(set) var species: [Species]? = nil
+    
+    func identify(img: UIImage, mediaId: UUID) async {
+        do {
+            try await BackendClient().upload(img: img, mediaId: mediaId.uuidString)
+            setSpeciesResult(results: try await BackendClient().imageId(mediaId: mediaId.uuidString))
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    private func setSpeciesResult(results: [SpeciesResult]) {
+        guard let path = Bundle.main.path(forResource: "strapi-db", ofType: "sqlite3") else {
+            preconditionFailure("Failed to find database file")
+        }
+        
+        let ids = results.map { $0.id }
+        
+        do {
+            let speciesDb = try Connection(path, readonly: true)
+            let query = Species.Definition.table
+                .filter(ids.contains(Species.Definition.id))
+            
+            do {
+                species = try speciesDb.prepareRowIterator(query).map { row in
+                    Species(id: row[Species.Definition.id],
+                            group: row[Species.Definition.group],
+                            sciname: row[Species.Definition.sciname],
+                            gername: row[Species.Definition.gername],
+                            engname: row[Species.Definition.engname],
+                            wikipedia: row[Species.Definition.wikipedia],
+                            maleUrl: row[Species.Definition.maleUrl],
+                            femaleUrl: row[Species.Definition.femaleUrl],
+                            gersynonym: row[Species.Definition.gersynonym],
+                            engsynonym: row[Species.Definition.engsynonym],
+                            redListGermany: row[Species.Definition.redListGermany],
+                            iucnCategory: row[Species.Definition.iucnCategory])
+                }
+            } catch {
+                preconditionFailure(error.localizedDescription)
+            }
+        } catch {
+            preconditionFailure(error.localizedDescription)
+        }
+    }
+}
+
+
 struct ResultView: SwiftUI.View {
     
     @Environment(\.dismiss) var dismiss
@@ -13,48 +64,12 @@ struct ResultView: SwiftUI.View {
     @SwiftUI.Binding var imageIdState: ImageIdState
     @SwiftUI.Binding var data: CreateData
     
-    @State var species: [Species]? = nil
-      
-    func setSpeciesResult(results: [SpeciesResult]) {
-      guard let path = Bundle.main.path(forResource: "strapi-db", ofType: "sqlite3") else {
-          preconditionFailure("Failed to find database file")
-      }
-      
-      let ids = results.map { $0.id }
-      
-      do {
-          let speciesDb = try Connection(path, readonly: true)
-          let query = Species.Definition.table
-              .filter(ids.contains(Species.Definition.id))
-          
-          do {
-              species = try speciesDb.prepareRowIterator(query).map { row in
-                  Species(id: row[Species.Definition.id],
-                          group: row[Species.Definition.group],
-                          sciname: row[Species.Definition.sciname],
-                          gername: row[Species.Definition.gername],
-                          engname: row[Species.Definition.engname],
-                          wikipedia: row[Species.Definition.wikipedia],
-                          maleUrl: row[Species.Definition.maleUrl],
-                          femaleUrl: row[Species.Definition.femaleUrl],
-                          gersynonym: row[Species.Definition.gersynonym],
-                          engsynonym: row[Species.Definition.engsynonym],
-                          redListGermany: row[Species.Definition.redListGermany],
-                          iucnCategory: row[Species.Definition.iucnCategory])
-              }
-          } catch {
-              preconditionFailure(error.localizedDescription)
-          }
-      } catch {
-          preconditionFailure(error.localizedDescription)
-      }
-    }
-      
+    @StateObject private var resultViewModel: ResultViewModel = ResultViewModel()
     
     var body: some SwiftUI.View {
         BaseView {
             VStack {
-                if let species = species {
+                if let species = resultViewModel.species {
                     ForEach(species) { species in
                         Button {
                             data.species = species
@@ -83,14 +98,10 @@ struct ResultView: SwiftUI.View {
         }
         .task {
             if let img = data.crop {
-                do {
-                    data.mediaId = UUID()
-                    if let mediaId = data.mediaId?.uuidString {
-                        try await BackendClient().upload(img: img, mediaId: mediaId)
-                        setSpeciesResult(results: try await BackendClient().imageId(mediaId: mediaId))
-                    }
-                } catch {
-                    fatalError(error.localizedDescription)
+                if resultViewModel.species == nil {
+                    let mediaId = UUID()
+                    data.mediaId = mediaId
+                    await resultViewModel.identify(img: img, mediaId: mediaId)
                 }
             }
         }
