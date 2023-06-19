c7efc2361e6982e151d86a9ecaf9ec8e4040ed8d
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2023-06-19 16:14:57 +0200

    Request permissions

--- naturblick/views/PlantIdView.swift
+++ naturblick/views/PlantIdView.swift
@@ -7,23 +7,40 @@ import SwiftUI
 
 struct PlantIdView: View {
     let client = BackendClient()
+    @StateObject private var cameraManager = CameraManager()
+    @StateObject private var photoLibraryManager = PhotoLibraryManager()
     @Binding var data: CreateData.ImageData
-
+    @State var authorized: Bool = false
     var body: some View {
-        if let crop = data.crop {
-            Text("Loading results")
+        if authorized {
+            if let crop = data.crop {
+                Text("Loading results")
+                    .task {
+                        do {
+                            try await client.upload(img: crop.image, mediaId: crop.id.uuidString)
+                            data.result = try await client.imageId(mediaId: crop.id.uuidString)
+                        } catch {
+                            preconditionFailure("\(error)")
+                        }
+                    }
+            } else if let image = data.image {
+                ImageCropper(image: image, crop: $data.crop)
+            } else {
+                ImagePickerView(data: $data)
+            }
+        } else {
+            Text("Naturblick requires permissions")
                 .task {
-                    do {
-                        try await client.upload(img: crop.image, mediaId: crop.id.uuidString)
-                        data.result = try await client.imageId(mediaId: crop.id.uuidString)
-                    } catch {
-                        preconditionFailure("\(error)")
+                    if cameraManager.askForPermission() {
+                        await cameraManager.requestAccess()
+                    }
+                    if photoLibraryManager.askForPermission() {
+                        await photoLibraryManager.requestAccess()
                     }
                 }
-        } else if let image = data.image {
-            ImageCropper(image: image, crop: $data.crop)
-        } else {
-            ImagePickerView(data: $data)
+                .onReceive(cameraManager.$isAuthorized.combineLatest(photoLibraryManager.$isAuthorized)) { camera, photo in
+                    authorized = camera && photo
+                }
         }
     }
 }
