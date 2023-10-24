af4c1a7a360bf4911b9d2470d261a58046a68d3a
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2023-10-24 15:33:59 +0200

    add available map icons

--- naturblick/models/Group.swift
+++ naturblick/models/Group.swift
@@ -58,8 +58,8 @@ extension String {
             return "map_spiders"
         case "actinopterygii":
             return "map_fish"
-        case "amphibian":
-            return "map_amphibian"
+    case "amphibian":
+        return "map_platform_amphibian"
         case "amphipoda":
             return "map_crustaceans"
         case "anaspidea":
@@ -68,8 +68,8 @@ extension String {
             return "map_spiders"
         case "araneae":
             return "map_spiders"
-        case "bird":
-            return "map_bird"
+    case "bird":
+        return "map_platform_bird"
         case "blattodea":
             return "map_insects"
         case "branchiobdellida":
@@ -78,16 +78,16 @@ extension String {
             return "map_crustaceans"
         case "bug":
             return "map_insects"
-        case "butterfly":
-            return "map_insects"
+    case "butterfly":
+        return "map_platform_butterfly"
         case "cephalaspidea":
             return "map_gastropoda"
         case "chilopoda":
             return "map_centipede"
         case "coleoptera":
             return "map_insects"
-        case "conifer":
-            return "map_tree"
+    case "conifer":
+        return "map_platform_conifer"
         case "crustacea":
             return "map_crustaceans"
         case "dermaptera":
@@ -104,8 +104,8 @@ extension String {
             return "map_gastropoda"
         case "grasshopper":
             return "map_insects"
-        case "herb":
-            return "map_herb"
+    case "herb":
+        return "map_platform_plant"
         case "heteroptera":
             return "map_insects"
         case "hirudinea":
@@ -113,11 +113,11 @@ extension String {
         case "hydrachnidia":
             return "map_spiders"
         case "hymenoptera":
-            return "map_insects"
+            return "map_platform_bee"
         case "lepidoptera":
             return "map_insects"
-        case "mammal":
-            return "map_mammal"
+    case "mammal":
+        return "map_platform_mammal"
         case "mantodea":
             return "map_insects"
         case "maxillopoda":
@@ -142,16 +142,16 @@ extension String {
             return "map_insects"
         case "raphidioptera":
             return "map_insects"
-        case "reptile":
-            return "map_reptile"
+    case "reptile":
+        return "map_platform_reptile"
         case "strigeida":
             return "map_bird"
         case "terebellida":
             return "map_ringworm"
         case "thysanoptera":
             return "map_insects"
-        case "tree":
-            return "map_tree"
+    case "tree":
+        return "map_platform_tree"
         case "trichoptera":
             return "map_insects"
         case "truebug":
@@ -163,57 +163,3 @@ extension String {
         }
     }
 }
-/*
-Pair("acarida", R.drawable.ic_spiders),
-            Pair("actinopterygii", R.drawable.ic_fish),
-            Pair("amphibian", R.drawable.ic_amphibian),
-            Pair("amphipoda", R.drawable.ic_crustaceans),
-            Pair("anaspidea", R.drawable.ic_gastropoda),
-            Pair("arachnid", R.drawable.ic_spiders),
-            Pair("araneae", R.drawable.ic_spiders),
-            Pair("bird", R.drawable.ic_bird),
-            Pair("blattodea", R.drawable.ic_insects),
-            Pair("branchiobdellida", R.drawable.ic_ringworm),
-            Pair("branchiopoda", R.drawable.ic_crustaceans),
-            Pair("bug", R.drawable.ic_insects),
-            Pair("butterfly", R.drawable.ic_insects),
-            Pair("cephalaspidea", R.drawable.ic_gastropoda),
-            Pair("chilopoda", R.drawable.ic_centipede),
-            Pair("coleoptera", R.drawable.ic_insects),
-            Pair("conifer", R.drawable.ic_tree),
-            Pair("crustacea", R.drawable.ic_crustaceans),
-            Pair("dermaptera", R.drawable.ic_insects),
-            Pair("diplopoda", R.drawable.ic_centipede),
-            Pair("diptera", R.drawable.ic_insects),
-            Pair("dragonfly", R.drawable.ic_insects),
-            Pair("ephemeroptera", R.drawable.ic_insects),
-            Pair("gastropoda", R.drawable.ic_gastropoda),
-            Pair("grasshopper", R.drawable.ic_insects),
-            Pair("herb", R.drawable.ic_herb),
-            Pair("heteroptera", R.drawable.ic_insects),
-            Pair("hirudinea", R.drawable.ic_ringworm),
-            Pair("hydrachnidia", R.drawable.ic_spiders),
-            Pair("hymenoptera", R.drawable.ic_insects),
-            Pair("lepidoptera", R.drawable.ic_insects),
-            Pair("mammal", R.drawable.ic_mammal),
-            Pair("mantodea", R.drawable.ic_insects),
-            Pair("maxillopoda", R.drawable.ic_crustaceans),
-            Pair("mecoptera", R.drawable.ic_insects),
-            Pair("megaloptera", R.drawable.ic_insects),
-            Pair("neuroptera", R.drawable.ic_insects),
-            Pair("odonata", R.drawable.ic_insects),
-            Pair("oligochaeta", R.drawable.ic_ringworm),
-            Pair("planipennia", R.drawable.ic_insects),
-            Pair("plecoptera", R.drawable.ic_insects),
-            Pair("polychaeta", R.drawable.ic_ringworm),
-            Pair("psocoptera", R.drawable.ic_insects),
-            Pair("raphidioptera", R.drawable.ic_insects),
-            Pair("reptile", R.drawable.ic_reptile),
-            Pair("strigeida", R.drawable.ic_bird),
-            Pair("terebellida", R.drawable.ic_ringworm),
-            Pair("thysanoptera", R.drawable.ic_insects),
-            Pair("tree", R.drawable.ic_tree),
-            Pair("trichoptera", R.drawable.ic_insects),
-            Pair("truebug", R.drawable.ic_insects),
-            Pair("zygentoma", R.drawable.ic_insects)
-*/
