f5ae27fe9bb505aade28c017a60761eacf6d6295
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2024-04-23 16:57:17 +0200

    Fix english search, order and synonym

--- naturblick/models/PickSpeciesListViewModel.swift
+++ naturblick/models/PickSpeciesListViewModel.swift
@@ -16,8 +16,8 @@ class PickSpeciesListViewModel: ObservableObject {
     func query(search: String, page: Int) throws -> [SpeciesListItem] {
         let searchString = searchOrNil(search: search)
         let query = Species.Definition.baseQuery
-        let queryWithSearch = searchString != nil ? query.filter(Species.Definition.gername.like(searchString!)) : query //TODO engname search as well
-        return try speciesDb.prepare(queryWithSearch.order(Species.Definition.gername).limit(PickSpeciesListViewModel.pageSize, offset: page * PickSpeciesListViewModel.pageSize))
+        let queryWithSearch = searchString != nil ? query.filter(Species.Definition.gername.like(searchString!) || Species.Definition.engname.like(searchString!)) : query
+        return try speciesDb.prepare(queryWithSearch.order(isGerman() ? Species.Definition.gername : Species.Definition.engname).limit(PickSpeciesListViewModel.pageSize, offset: page * PickSpeciesListViewModel.pageSize))
             .map { row in
                 SpeciesListItem(
                     speciesId: row[Species.Definition.table[Species.Definition.id]],
