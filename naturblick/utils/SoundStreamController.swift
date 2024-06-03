0768f8ab5d41b4e300e518733cf8092173c3c1a2
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2024-06-03 16:08:31 +0200

    Simplify audio session handling and reset category

--- naturblick/utils/SoundStreamController.swift
+++ naturblick/utils/SoundStreamController.swift
@@ -20,6 +20,8 @@ class SoundStreamController : ObservableObject {
     }
     
     @MainActor func play(url: URL){
+        // Always use solo ambient (default) for playback, could have been changed by the sound recorder
+        try? AVAudioSession.sharedInstance().setCategory(.soloAmbient)
         self.audioPlayer = AVPlayer(url: url)
         self.audioPlayer?.play()
         self.audioPlayer?.publisher(for: \.timeControlStatus)
